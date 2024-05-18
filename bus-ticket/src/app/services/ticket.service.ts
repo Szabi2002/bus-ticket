164ac441648a845d815231d99912a8114b26dac3
Nagynemes Szabolcs <h162202@stud.u-szeged.hu>
2024-05-18 18:33:05 +0200

    fix: ticket.service

--- bus-ticket/src/app/services/ticket.service.ts
+++ bus-ticket/src/app/services/ticket.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Ticket } from '../models/Ticket';
 import { user } from '@angular/fire/auth';
 import { ref } from '@angular/fire/storage';
